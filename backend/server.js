58c3f9bcf00fb1919b437fb8a76075c74f115c3f
aradevgc <arangrancla@gmail.com>
2026-03-26 17:04:38 +0100

    update

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const { setupWebSocket } = require("./websocket");
 
+
 const server = http.createServer((req, res) => {
   res.writeHead(200);
   res.end("Servidor activo");
@@ -12,3 +13,9 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
 });
+
+const express = require('express'); // Si usas express
+const path = require('path');
+
+// Esto sirve los archivos estáticos del frontend
+server.use(express.static(path.join(__dirname, '../frontend')));
